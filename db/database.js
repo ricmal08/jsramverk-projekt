fbe81ab9b81deb4027652a5e433847e17eb0aaf9
Jakob <jahl24@student.bth.se>
2025-09-12 17:03:33 +0200

    Added MongoDB connection setup with installations in package.json

---
+++ db/database.js
@@ -0,0 +1,36 @@
+/**
+ * Connect to the database MongoDB.
+ */
+
+const mongo = require("mongodb").MongoClient;
+const collectionName = "documents"
+
+
+const database = {
+    getDb: async function getDb() {
+        let dsn = "mongodb://localhost:27017";
+        let dbName = "documents"
+
+        if (process.env.NODE_ENV === 'test') {
+            dbName = "test";
+        }
+
+        try {
+            const client  = await mongo.connect(dsn);
+            const db = await client.db(dbName);
+            const collection = await db.collection(collectionName);
+
+            return {
+                db: db,
+                collection: collection,
+                client: client,
+            };
+        } catch (error) {
+            console.error("Database connection failed:", error);
+            throw error;
+        }    
+
+    }
+}
+
+module.exports = database;
